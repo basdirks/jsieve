da1a6e4ce438beb6bda8458098039c235f62f5d0
Base Disk <i@basdirks.com>
2014-11-06 17:04:16 +0100

    Uint8Array

--- primes.js
+++ primes.js
@@ -1,5 +1,5 @@
 module.exports = function (n) {
-    var i, j, k, m, s = {}, p = [2], z = Math.floor(Math.sqrt(n));
+    var i, j, k, m, s = new Int8Array(n), p = [2], z = Math.floor(Math.sqrt(n));
 
     for (i = 3; i < n; i += 2) {
         s[i] = true;
