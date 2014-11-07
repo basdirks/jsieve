c9effd3937b75cf376d98e37bc250e19da2f2ec1
Base Disk <i@basdirks.com>
2014-11-07 10:35:12 +0100

    Use bit-array

--- primes.js
+++ primes.js
@@ -1,24 +1,28 @@
-module.exports = function (n) {
-    var i, j, k, m, s = new Int8Array(n), p = [2], z = Math.floor(Math.sqrt(n));
+var BitArray = require('bit-array');
+
+function sieve (n) {
+    var i, j, k, m, s = new BitArray(n + 1), p = [2], z = Math.floor(Math.sqrt(n));
 
     for (i = 3; i < n; i += 2) {
-        s[i] = true;
+        s.set(i, true);
     }
 
     for (j = 0; z > j; j++) {
-        if (s[j]) {
+        if (s.get(j)) {
             p.push(j);
             for (k = j * 2; k <= n; k += j) {
-                s[k] = false;
+                s.set(k, false);
             }
         }
     }
 
     for (m = z; m < n; m++) {
-        if (s[m]) {
+        if (s.get(m)) {
             p.push(m);
         }
     }
 
     return p;
 }
+
+console.log(sieve(1e9).length);
